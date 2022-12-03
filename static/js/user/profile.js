37993fe425d4683825177f7ff6e8909a7e7f9085
mgd <mgd2002zzz@sjtu.edu.cn>
2022-12-03 21:02:31 +0800

    profile

--- static/js/user/profile.js
+++ static/js/user/profile.js
@@ -3,8 +3,10 @@ document.getElementById("curr-year").innerHTML = date.getFullYear();
 function show(){
     var btnobj=document.getElementById("btn1");
     var navobj=document.getElementById("nav1-1");
+    var navobj2=document.getElementById("btn2");
     var divobj1=document.getElementById("show1");
     var divobj2=document.getElementById("show2");
+    var divobj3=document.getElementById("show3");
 
     if(btnobj.value=="显示"){
 
@@ -12,27 +14,57 @@ function show(){
     }else{
         divobj1.style.display="none";
         divobj2.style.display="block";
+        divobj3.style.display="none";
         btnobj.value="显示";
         navobj.value="隐藏";
+        navobj2.value="隐藏";
         $("#p-tabs-m #nav1-1").removeClass("active");
         $("#p-tabs-m #btn1").addClass("active");
+        $("#p-tabs-m #btn2").removeClass("active");
     }
 }
 function show1(){
     var btnobj=document.getElementById("nav1-1");
     var navobj=document.getElementById("btn1");
+    var navobj2=document.getElementById("btn2");
     var divobj1=document.getElementById("show1");
     var divobj2=document.getElementById("show2");
-
+    var divobj3=document.getElementById("show3");
     if(btnobj.value=="显示"){
 
 
     }else{
         divobj2.style.display="none";
         divobj1.style.display="block";
+        divobj3.style.display="none";
         btnobj.value="显示";
         navobj.value="隐藏";
+        navobj2.value="隐藏";
         $("#p-tabs-m #nav1-1").addClass("active");
         $("#p-tabs-m #btn1").removeClass("active");
+        $("#p-tabs-m #btn2").removeClass("active");
+    }
+}
+
+function show2(){
+    var navobj2=document.getElementById("nav1-1");
+    var navobj=document.getElementById("btn1");
+    var btnobj=document.getElementById("btn2");
+    var divobj1=document.getElementById("show1");
+    var divobj2=document.getElementById("show2");
+    var divobj3=document.getElementById("show3");
+    if(btnobj.value=="显示"){
+
+
+    }else{
+        divobj1.style.display="none";
+        divobj2.style.display="none";
+        divobj3.style.display="block";
+        btnobj.value="显示";
+        navobj.value="隐藏";
+        navobj2.value="隐藏";
+        $("#p-tabs-m #btn2").addClass("active");
+        $("#p-tabs-m #btn1").removeClass("active");
+        $("#p-tabs-m #nav1-1").removeClass("active");
     }
 }
