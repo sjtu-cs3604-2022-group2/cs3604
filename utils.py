14c4da1776d26f3e1eca9166608c61e7cc2aaad0
dai-yutong <1161286210@qq.com>
2022-12-11 20:10:02 +0800

    调整图片大小

--- utils.py
+++ utils.py
@@ -77,6 +77,8 @@ def get_recommendation_users(user_id):
 def filter_body_content(str):
     reg_p = re.compile(r"</p>")
     reg_tag = re.compile(r"<.+?>")
+    reg_img = re.compile(r"<\s*img.+?>")
+    str = reg_img.sub("[图片]", str)
     str = reg_p.sub("   ", str)
     str = reg_tag.sub("", str)
     i=0
