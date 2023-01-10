ee61524f0df2ff2894506c7f3857f028ea0fcb76
mgd <mgd2002zzz@sjtu.edu.cn>
2023-01-10 23:56:26 +0800

    models单元测试

--- test_models.py
+++ test_models.py
@@ -1,4 +1,4 @@
-from models import User
+from models import *
 from app import *
 import unittest
 from extensions import db
@@ -28,6 +28,8 @@ class ModelsTest(unittest.TestCase):
         self.assertEqual(user_reg1.password, "123")
         self.assertEqual(user_reg1.email, "test_reg1@123.com")
         self.assertEqual(user_reg1.about, "test_reg1")
+        flag = user_reg1.is_admin
+        self.assertEqual(flag, False)       
 
         db.session.delete(user_reg1)
         db.session.commit()
@@ -64,6 +66,10 @@ class ModelsTest(unittest.TestCase):
         user_followed = User.query.filter(User.username == "test_follow_user2").first()
         # 关注user
         flag = user_follow.follow(user_followed)
+        self.assertEqual(flag, True)
+        flag = user_follow.is_following(user_followed)
+        self.assertEqual(flag, True)
+        flag = user_followed.is_followed_by(user_follow)
         self.assertEqual(flag, True)
 
     # 测试关注，但是关注的用户已关注（此时关注应该失败）
@@ -117,5 +123,48 @@ class ModelsTest(unittest.TestCase):
         db.session.delete(user_unfollow)
         db.session.delete(user_unfollowed)
         db.session.commit()
+    
+    # 测试收藏功能
+    def test_favor(self):
+
+        user = User(
+            username="test_favor", password=123, email="test@123.com",about="test",image='#'
+        )
+        # 测试user创建收藏夹功能
+        new_name = "新的收藏夹"
+        collection = FavoriteCollection(user=user, name=new_name)
+        db.session.add(collection)
+        db.session.commit()
+        # 把post加入相应收藏夹
+        post_id=1
+        post = Post.query.get(post_id)
+        user.add_favorite(post, collection)
+        # 测试post是否在收藏夹中
+        flag=user.in_favorite(post, collection=collection)
+        self.assertEqual(flag, True)
+        # 删除收藏夹中的post
+        user.remove_favorite(post, collection=collection)
+        flag=user.in_favorite(post, collection=collection)
+        self.assertEqual(flag, False)
+        # 再次加入收藏夹
+        post_id=1
+        post = Post.query.get(post_id)
+        user.add_favorite(post, collection)
+        # 测试是否在收藏夹中，但是没有collection参数
+        flag = user.in_favorite(post)
+        self.assertEqual(flag, True)
+        # 测试收藏已收藏的post
+        flag = user.add_favorite(post, collection)
+        self.assertEqual(flag, False)
+        # 测试删除post，但是没有collection参数
+        flag = user.remove_favorite(post)
+        self.assertEqual(flag, True)
+        flag = user.in_favorite(post)
+        self.assertEqual(flag, False)
+
+
+        db.session.delete(user)
+        db.session.commit()
+
 if __name__ == '__main__':
     unittest.main()
